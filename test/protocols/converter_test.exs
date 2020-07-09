0d69cc7295df129717e418755421afd5963e77ad
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-09 18:40:23 -0300

    repo(Converter): refactor Converter from a Protocol into a Module

--- test/protocols/converter_test.exs
+++ test/protocols/converter_test.exs
@@ -11,169 +11,169 @@ defmodule ConverterTest do
     describe "convert/1 for String" do
 
         test "should return equivalent values for combinations of I" do
-            assert Converter.convert("I") == 1
-            assert Converter.convert("II") == 2
-            assert Converter.convert("III") == 3
+            assert Converter.from_roman("I") == 1
+            assert Converter.from_roman("II") == 2
+            assert Converter.from_roman("III") == 3
         end
 
         test "should throw an exception in case of a invalid combinations of I" do
-            assert catch_error(Converter.convert("IVIII")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IVIII")) == roman_position_error()
         end
 
         test "should return equivalent values for combinations of V Roman number" do
-            assert Converter.convert("V") == 5
-            assert Converter.convert("IV") == 4
-            assert Converter.convert("VIII") == 8
+            assert Converter.from_roman("V") == 5
+            assert Converter.from_roman("IV") == 4
+            assert Converter.from_roman("VIII") == 8
         end
 
         test "should throw an exception in case of a invalid combinations of V" do
-            assert catch_error(Converter.convert("IIV")) == roman_position_error()
-            assert catch_error(Converter.convert("IIIV")) == roman_position_error()
-            assert catch_error(Converter.convert("VV")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IIV")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IIIV")) == roman_position_error()
+            assert catch_error(Converter.from_roman("VV")) == roman_position_error()
         end
 
         test "should return equivalent values for combinations of X Roman number" do
-            assert Converter.convert("IX") == 9
-            assert Converter.convert("X") == 10
-            assert Converter.convert("XI") == 11
-            assert Converter.convert("XII") == 12
-            assert Converter.convert("XIII") == 13
-            assert Converter.convert("XIV") == 14
-            assert Converter.convert("XV") == 15
-            assert Converter.convert("XX") == 20
-            assert Converter.convert("XXX") == 30
-            assert Converter.convert("XXXI") == 31
-            assert Converter.convert("XXXII") == 32
-            assert Converter.convert("XXXIII") == 33
-            assert Converter.convert("XXXIV") == 34
-            assert Converter.convert("XXXV") == 35
+            assert Converter.from_roman("IX") == 9
+            assert Converter.from_roman("X") == 10
+            assert Converter.from_roman("XI") == 11
+            assert Converter.from_roman("XII") == 12
+            assert Converter.from_roman("XIII") == 13
+            assert Converter.from_roman("XIV") == 14
+            assert Converter.from_roman("XV") == 15
+            assert Converter.from_roman("XX") == 20
+            assert Converter.from_roman("XXX") == 30
+            assert Converter.from_roman("XXXI") == 31
+            assert Converter.from_roman("XXXII") == 32
+            assert Converter.from_roman("XXXIII") == 33
+            assert Converter.from_roman("XXXIV") == 34
+            assert Converter.from_roman("XXXV") == 35
         end
 
         test "should throw an exception in case of a invalid combinations of X" do
-            assert catch_error(Converter.convert("IIX")) == roman_position_error()
-            assert catch_error(Converter.convert("VX")) == roman_position_error()
-            assert catch_error(Converter.convert("VIX")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IIX")) == roman_position_error()
+            assert catch_error(Converter.from_roman("VX")) == roman_position_error()
+            assert catch_error(Converter.from_roman("VIX")) == roman_position_error()
         end
 
         test "should return equivalent values for combinations of L Roman number" do
-            assert Converter.convert("L") == 50
-            assert Converter.convert("XL") == 40
-            assert Converter.convert("LX") == 60
-            assert Converter.convert("LXX") == 70
-            assert Converter.convert("LXXX") == 80
+            assert Converter.from_roman("L") == 50
+            assert Converter.from_roman("XL") == 40
+            assert Converter.from_roman("LX") == 60
+            assert Converter.from_roman("LXX") == 70
+            assert Converter.from_roman("LXXX") == 80
         end
 
         test "should throw an exception in case of a invalid combinations of L" do
-            assert catch_error(Converter.convert("IIL")) == roman_position_error()
-            assert catch_error(Converter.convert("XXXL")) == roman_position_error()
-            assert catch_error(Converter.convert("LL")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IIL")) == roman_position_error()
+            assert catch_error(Converter.from_roman("XXXL")) == roman_position_error()
+            assert catch_error(Converter.from_roman("LL")) == roman_position_error()
         end
 
         test "should return equivalent values for combinations of C Roman number" do
-            assert Converter.convert("XC") == 90
-            assert Converter.convert("C") == 100
-            assert Converter.convert("CX") == 110
-            assert Converter.convert("CXX") == 120
-            assert Converter.convert("CXXX") == 130
-            assert Converter.convert("CXL") == 140
-            assert Converter.convert("CL") == 150
-            assert Converter.convert("CC") == 200
-            assert Converter.convert("CCC") == 300
-            assert Converter.convert("CCCI") == 301
-            assert Converter.convert("CCCX") == 310
-            assert Converter.convert("CCCXX") == 320
-            assert Converter.convert("CCCXXX") == 330
-            assert Converter.convert("CCCXL") == 340
-            assert Converter.convert("CCCL") == 350
+            assert Converter.from_roman("XC") == 90
+            assert Converter.from_roman("C") == 100
+            assert Converter.from_roman("CX") == 110
+            assert Converter.from_roman("CXX") == 120
+            assert Converter.from_roman("CXXX") == 130
+            assert Converter.from_roman("CXL") == 140
+            assert Converter.from_roman("CL") == 150
+            assert Converter.from_roman("CC") == 200
+            assert Converter.from_roman("CCC") == 300
+            assert Converter.from_roman("CCCI") == 301
+            assert Converter.from_roman("CCCX") == 310
+            assert Converter.from_roman("CCCXX") == 320
+            assert Converter.from_roman("CCCXXX") == 330
+            assert Converter.from_roman("CCCXL") == 340
+            assert Converter.from_roman("CCCL") == 350
         end
 
         test "should throw an exception in case of a invalid combinations of C" do
-            assert catch_error(Converter.convert("IC")) == roman_position_error()
-            assert catch_error(Converter.convert("VC")) == roman_position_error()
-            assert catch_error(Converter.convert("IXC")) == roman_position_error()
-            assert catch_error(Converter.convert("XXC")) == roman_position_error()
-            assert catch_error(Converter.convert("LC")) == roman_position_error()
-            assert catch_error(Converter.convert("LXC")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IC")) == roman_position_error()
+            assert catch_error(Converter.from_roman("VC")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IXC")) == roman_position_error()
+            assert catch_error(Converter.from_roman("XXC")) == roman_position_error()
+            assert catch_error(Converter.from_roman("LC")) == roman_position_error()
+            assert catch_error(Converter.from_roman("LXC")) == roman_position_error()
         end
 
         test "should return equivalent values for combinations of D Roman number" do
-            assert Converter.convert("D") == 500
-            assert Converter.convert("DI") == 501
-            assert Converter.convert("DX") == 510
-            assert Converter.convert("CD") == 400
-            assert Converter.convert("DC") == 600
-            assert Converter.convert("DCC") == 700
-            assert Converter.convert("DCCC") == 800
+            assert Converter.from_roman("D") == 500
+            assert Converter.from_roman("DI") == 501
+            assert Converter.from_roman("DX") == 510
+            assert Converter.from_roman("CD") == 400
+            assert Converter.from_roman("DC") == 600
+            assert Converter.from_roman("DCC") == 700
+            assert Converter.from_roman("DCCC") == 800
         end
 
         test "should throw an exception in case of a invalid combinations of D" do
-            assert catch_error(Converter.convert("ID")) == roman_position_error()
-            assert catch_error(Converter.convert("XD")) == roman_position_error()
-            assert catch_error(Converter.convert("XCD")) == roman_position_error()
-            assert catch_error(Converter.convert("CCCD")) == roman_position_error()
-            assert catch_error(Converter.convert("DD")) == roman_position_error()
+            assert catch_error(Converter.from_roman("ID")) == roman_position_error()
+            assert catch_error(Converter.from_roman("XD")) == roman_position_error()
+            assert catch_error(Converter.from_roman("XCD")) == roman_position_error()
+            assert catch_error(Converter.from_roman("CCCD")) == roman_position_error()
+            assert catch_error(Converter.from_roman("DD")) == roman_position_error()
         end
 
         test "should return equivalent values for combinations of M Roman number" do
-            assert Converter.convert("CM") == 900
-            assert Converter.convert("M") == 1000
-            assert Converter.convert("MC") == 1100
-            assert Converter.convert("MCC") == 1200
-            assert Converter.convert("MCCC") == 1300
-            assert Converter.convert("MCD") == 1400
-            assert Converter.convert("MD") == 1500
-            assert Converter.convert("MM") == 2000
-            assert Converter.convert("MMM") == 3000
-            assert Converter.convert("MMMI") == 3001
-            assert Converter.convert("MMMX") == 3010
-            assert Converter.convert("MMMC") == 3100
-            assert Converter.convert("MMMCC") == 3200
-            assert Converter.convert("MMMCCC") == 3300
-            assert Converter.convert("MMMCD") == 3400
-            assert Converter.convert("MMMD") == 3500
+            assert Converter.from_roman("CM") == 900
+            assert Converter.from_roman("M") == 1000
+            assert Converter.from_roman("MC") == 1100
+            assert Converter.from_roman("MCC") == 1200
+            assert Converter.from_roman("MCCC") == 1300
+            assert Converter.from_roman("MCD") == 1400
+            assert Converter.from_roman("MD") == 1500
+            assert Converter.from_roman("MM") == 2000
+            assert Converter.from_roman("MMM") == 3000
+            assert Converter.from_roman("MMMI") == 3001
+            assert Converter.from_roman("MMMX") == 3010
+            assert Converter.from_roman("MMMC") == 3100
+            assert Converter.from_roman("MMMCC") == 3200
+            assert Converter.from_roman("MMMCCC") == 3300
+            assert Converter.from_roman("MMMCD") == 3400
+            assert Converter.from_roman("MMMD") == 3500
         end
 
         test "should throw an exception in case of a invalid combinations of M" do
-            assert catch_error(Converter.convert("IM")) == roman_position_error()
-            assert catch_error(Converter.convert("VM")) == roman_position_error()
-            assert catch_error(Converter.convert("XM")) == roman_position_error()
-            assert catch_error(Converter.convert("LM")) == roman_position_error()
-            assert catch_error(Converter.convert("DM")) == roman_position_error()
-            assert catch_error(Converter.convert("XCM")) == roman_position_error()
-            assert catch_error(Converter.convert("CCM")) == roman_position_error()
-            assert catch_error(Converter.convert("DCM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("IM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("VM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("XM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("LM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("DM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("XCM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("CCM")) == roman_position_error()
+            assert catch_error(Converter.from_roman("DCM")) == roman_position_error()
         end
 
         test "tests for complex Roman numbers" do
-            assert Converter.convert("XIII") == 13
-            assert Converter.convert("XIV") == 14
-            assert Converter.convert("XVII") == 17
-            assert Converter.convert("XIX") == 19
-            assert Converter.convert("XXXIII") == 33
-            assert Converter.convert("XXXVII") == 37
-            assert Converter.convert("XLIII") == 43
-            assert Converter.convert("XLIV") == 44
-            assert Converter.convert("XLV") == 45
-            assert Converter.convert("LVI") == 56
-            assert Converter.convert("LXXVI") == 76
-            assert Converter.convert("LXXXVIII") == 88
-            assert Converter.convert("XCIV") == 94
-            assert Converter.convert("XCIX") == 99
-            assert Converter.convert("CLXXXVIII") == 188
-            assert Converter.convert("CCXXII") == 222
-            assert Converter.convert("CCCXXXIII") == 333
-            assert Converter.convert("CCCLXXXVIII") == 388
-            assert Converter.convert("CDXLIV") == 444
-            assert Converter.convert("DLV") == 555
-            assert Converter.convert("DCCLXXVII") == 777
-            assert Converter.convert("DCCCLXXXVIII") == 888
-            assert Converter.convert("CMXCIX") == 999
-            assert Converter.convert("MCCCXXXVIII") == 1338
-            assert Converter.convert("MMCCCXLV") == 2345
-            assert Converter.convert("MMDCLXVI") == 2666
-            assert Converter.convert("MMDCCCLXXXVIII") == 2888
-            assert Converter.convert("MMCMLXXXVII") == 2987
-            assert Converter.convert("MMCMXCIX") == 2999
+            assert Converter.from_roman("XIII") == 13
+            assert Converter.from_roman("XIV") == 14
+            assert Converter.from_roman("XVII") == 17
+            assert Converter.from_roman("XIX") == 19
+            assert Converter.from_roman("XXXIII") == 33
+            assert Converter.from_roman("XXXVII") == 37
+            assert Converter.from_roman("XLIII") == 43
+            assert Converter.from_roman("XLIV") == 44
+            assert Converter.from_roman("XLV") == 45
+            assert Converter.from_roman("LVI") == 56
+            assert Converter.from_roman("LXXVI") == 76
+            assert Converter.from_roman("LXXXVIII") == 88
+            assert Converter.from_roman("XCIV") == 94
+            assert Converter.from_roman("XCIX") == 99
+            assert Converter.from_roman("CLXXXVIII") == 188
+            assert Converter.from_roman("CCXXII") == 222
+            assert Converter.from_roman("CCCXXXIII") == 333
+            assert Converter.from_roman("CCCLXXXVIII") == 388
+            assert Converter.from_roman("CDXLIV") == 444
+            assert Converter.from_roman("DLV") == 555
+            assert Converter.from_roman("DCCLXXVII") == 777
+            assert Converter.from_roman("DCCCLXXXVIII") == 888
+            assert Converter.from_roman("CMXCIX") == 999
+            assert Converter.from_roman("MCCCXXXVIII") == 1338
+            assert Converter.from_roman("MMCCCXLV") == 2345
+            assert Converter.from_roman("MMDCLXVI") == 2666
+            assert Converter.from_roman("MMDCCCLXXXVIII") == 2888
+            assert Converter.from_roman("MMCMLXXXVII") == 2987
+            assert Converter.from_roman("MMCMXCIX") == 2999
         end
     end
 end
