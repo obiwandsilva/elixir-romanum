674f879f65e2e3a1118e0423ab2f1bb6753fe516
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-09 21:33:26 -0300

    repo(Input): ask for user input when running the application

--- lib/modules/input.ex
+++ lib/modules/input.ex
@@ -1,6 +1,29 @@
 defmodule Input do
+
+    defp message(nil, source) do
+        "#{source} -> Invalid value"
+    end
+    
+    defp message(result, source) do
+        "#{source} -> #{result}"
+    end
+
     def ask() do
-        input = IO.gets("Enter a valid Roman number\n")
+        input = IO.gets("Enter a valid Roman or decimal number or type 'exit' to quit:\n")
         cleaned = String.replace(input, ~r/(\n|\s)/, "")
+
+        try do
+            if cleaned == "exit" do
+                IO.puts("Exiting Romanum...")
+            else
+                Romanum.convert(cleaned)
+                |> message(cleaned)
+                |> IO.puts
+                
+                ask()
+            end
+        rescue
+            error -> IO.puts(error)
+        end
     end
 end
--- lib/romanum/application.ex
+++ lib/romanum/application.ex
@@ -2,7 +2,7 @@ defmodule Romanum.Application do
     use Application
 
     def start(_type, _args) do
-        IO.puts("Romanum is running...")
+        IO.puts("Starting Romanum...")
         timeout = 180000
 
         task = Task.async(&Input.ask/0)
