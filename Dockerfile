32d65dd744e463ba05c2971558e9b600685e23df
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-09 23:49:04 -0300

    chore(Dockerfile): add a Dockerfile and its deps to the project

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM elixir:1.10.4-alpine
+WORKDIR /tmp/romanum
+COPY . .
+COPY romanum /bin
+RUN chmod +x /bin/romanum
+ENV MIX_ENV prod
+RUN mix release --version 1.0.0 --force
--- mix.exs
+++ mix.exs
@@ -4,11 +4,17 @@ defmodule Romanum.MixProject do
   def project do
     [
       app: :romanum,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: [
+        romanum: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
